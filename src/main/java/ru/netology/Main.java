c81258f074d9f3c0371a38f0cbe0aa553a25e646
Vladislav <nikolaenko_vladislav@icloud.com>
2022-10-12 09:38:00 +0300

    hid 'target', and fixed soketProblem

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,11 +1,13 @@
 package ru.netology;
 
+import java.io.IOException;
+
 public class Main {
 
     final static int COUNT_POOL = 64;
     final static int PORT = 9999;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Server server = new Server(COUNT_POOL);
         server.startServer(PORT);
